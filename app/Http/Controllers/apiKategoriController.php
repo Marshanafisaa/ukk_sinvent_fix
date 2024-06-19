9af32d9b027395e33b95e3f4899fc6550d92fe88
Marshanafisaa <marshanafisaputri@gmail.com>
2024-06-19 20:28:00 +0700

    [invent-03] Detail Salah Satu Kategori

--- app/Http/Controllers/apiKategoriController.php
+++ app/Http/Controllers/apiKategoriController.php
@@ -42,7 +42,14 @@ class apiKategoriController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $kategori = Kategori::find($id);
+        
+        if(!$kategori){
+            return response()->json(['message' => 'Kategori tidak ditemukan'], 404);
+        }else{
+            $data=array("data"=>$kategori);
+            return response()->json($data);
+        }
     }
 
     /**
